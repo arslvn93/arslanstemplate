89a568772fdf77eb060aaaa7e7e53a1d7fa852d3
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-03-21 05:07:34 -0400

    remove property sections and organize by type

--- app/page.jsx
+++ app/page.jsx
@@ -26,10 +26,8 @@ import Contact7 from "@/components/homes/home-7/Contact";
 // Home 10 Imports
 // ----------------------------
 import Brands10 from "@/components/homes/home-10/Brands";
-import Properties10 from "@/components/homes/home-10/Properties";
-import Properties2_10 from "@/components/homes/home-10/Properties2";
-import Properties3_10 from "@/components/homes/home-10/Properties3";
 import HelpSection10 from "@/components/homes/home-10/HelpSection";
+// (Removed: Properties10, Properties2_10, Properties3_10)
 
 // ----------------------------
 // Home 1 Imports
@@ -47,13 +45,13 @@ import Cta1 from "@/components/homes/home-1/Cta";
 import DownloadApp1 from "@/components/homes/home-1/DownloadApp";
 import Facts1 from "@/components/homes/home-1/Facts";
 import Featured1 from "@/components/homes/home-1/Featured";
-import FeaturedProperty1 from "@/components/homes/home-1/FeaturedProperty";
+// (Removed: FeaturedProperty1)
 import Features1 from "@/components/homes/home-1/Features";
 import HelpCenter1 from "@/components/homes/home-1/HelpCenter";
 import HelpSection1 from "@/components/homes/home-1/HelpSection";
 import LoanCalculator1 from "@/components/homes/home-1/LoanCalculator";
 import Projects1 from "@/components/homes/home-1/Projects";
-import Properties1 from "@/components/homes/home-1/Properties";
+// (Removed: Properties1)
 
 // ----------------------------
 // Home 2 Imports
@@ -63,50 +61,49 @@ import Header2 from "@/components/headers/Header2";
 import Home2Hero from "@/components/homes/home-2/Hero";
 import Home2About from "@/components/homes/home-2/About";
 import Home2Agents from "@/components/homes/home-2/Agents";
+// (Removed: Home2Properties)
 import Home2Banner from "@/components/homes/home-2/Banner";
 import Home2Blogs from "@/components/homes/home-2/Blogs";
 import Home2Brands from "@/components/homes/home-2/Brands";
 import Home2Categories from "@/components/homes/home-2/Categories";
 import Home2Cities from "@/components/homes/home-2/Cities";
 import Home2Facts from "@/components/homes/home-2/Facts";
-import Home2Properties from "@/components/homes/home-2/Properties";
 import Home2Testimonials from "@/components/homes/home-2/Testimonials";
 
 // ----------------------------
 // Home 8 Imports
 // ----------------------------
 import Header8 from "@/components/headers/Header8";
+import Home8Hero from "@/components/homes/home-8/Hero";
 import Home8Agents from "@/components/homes/home-8/Agents";
 import Home8Banner from "@/components/homes/home-8/Banner";
 import Home8Blogs from "@/components/homes/home-8/Blogs";
 import Home8Brands from "@/components/homes/home-8/Brands";
 import Home8Facts from "@/components/homes/home-8/Facts";
-import Home8FeaturedProperty from "@/components/homes/home-8/FeaturedProperty";
-import Home8Hero from "@/components/homes/home-8/Hero";
-import Home8Properties from "@/components/homes/home-8/Properties";
 import Home8Services from "@/components/homes/home-8/Services";
 import Home8Testimonials from "@/components/homes/home-8/Testimonials";
 import Home8WelcomeSection from "@/components/homes/home-8/WelcomeSection";
+// (Removed: Home8FeaturedProperty, Home8Properties)
 
 // ----------------------------
 // Home 9 Imports
 // ----------------------------
 import Header9 from "@/components/headers/Header9";
+import Home9Hero from "@/components/homes/home-9/Hero";
 import Home9About from "@/components/homes/home-9/About";
 import Home9Agents from "@/components/homes/home-9/Agents";
 import Home9Blogs from "@/components/homes/home-9/Blogs";
 import Home9Brands from "@/components/homes/home-9/Brands";
 import Home9Facts from "@/components/homes/home-9/Facts";
-import Home9Features from "@/components/homes/home-9/Features";
-import Home9Hero from "@/components/homes/home-9/Hero";
-import Home9Properties from "@/components/homes/home-9/Properties";
 import Home9Services from "@/components/homes/home-9/Services";
 import Home9Services2 from "@/components/homes/home-9/Services2";
+import Home9Features from "@/components/homes/home-9/Features";
 import Home9Testimonials from "@/components/homes/home-9/Testimonials";
+// (Removed: Home9Properties)
 
 export const metadata = {
   title: "Combined Real Estate Website",
-  description: "Combined Real Estate Website with all home sections",
+  description: "Combined Real Estate Website with all home sections (excluding property ones)",
 };
 
 export default function HomePage() {
@@ -114,123 +111,133 @@ export default function HomePage() {
     <>
       <ThemeController themeColor="default-dark" />
       <div id="wrapper" className="bg-body-1 counter-scroll">
+
         {/* ---------------------------- */}
-        {/* Home 6 Section */}
+        {/* Headers */}
         {/* ---------------------------- */}
-        <Header6 />
-        <Hero6 />
-        <div className="main-content">
+        <section id="headers">
+          <Header6 />
+          <Header2 />
+          <Header8 />
+          <Header9 />
+        </section>
+
+        {/* ---------------------------- */}
+        {/* Heroes */}
+        {/* ---------------------------- */}
+        <section id="heroes">
+          <Hero6 />
+          <Home2Hero />
+          <Home8Hero />
+          <Home9Hero />
+          <Hero1 />
+        </section>
+
+        {/* ---------------------------- */}
+        {/* About / Introduction */}
+        {/* ---------------------------- */}
+        <section id="about">
           <About6 />
+          <Home2About />
+          <Home9About />
+          <About1 />
+          <Home8WelcomeSection />
+        </section>
+
+        {/* ---------------------------- */}
+        {/* Services / Features */}
+        {/* ---------------------------- */}
+        <section id="services">
           <Services6 />
+          <Home9Services />
+          <Home9Features />
+          <Home9Services2 />
+          <Home8Services />
+          <Features1 />
+        </section>
+
+        {/* ---------------------------- */}
+        {/* Agents */}
+        {/* ---------------------------- */}
+        <section id="agents">
+          <Agents1 />
+          <Home2Agents />
+          <Home8Agents />
+          <Home9Agents />
+        </section>
+
+        {/* ---------------------------- */}
+        {/* Testimonials */}
+        {/* ---------------------------- */}
+        <section id="testimonials">
+          <Testimonials6 />
+          <Home2Testimonials />
+          <Home8Testimonials />
+          <Home9Testimonials />
+        </section>
+
+        {/* ---------------------------- */}
+        {/* Blogs */}
+        {/* ---------------------------- */}
+        <section id="blogs">
+          <Blogs6 />
+          <Home2Blogs />
+          <Home8Blogs />
+          <Home9Blogs />
+          <Blogs1 />
+        </section>
+
+        {/* ---------------------------- */}
+        {/* Miscellaneous / Other Content */}
+        {/* ---------------------------- */}
+        <section id="misc">
+          {/* Home 6 Content */}
           <Projects6 />
           <Team6 />
-          <Testimonials6 />
-          <Blogs6 />
-          <Contact6 />
-        </div>
-
-        {/* ---------------------------- */}
-        {/* Home 7 Section */}
-        {/* ---------------------------- */}
-        <HighlightSection />
-        <HelpCenter />
-        <Contact7 />
-
-        {/* ---------------------------- */}
-        {/* Home 10 Section */}
-        {/* ---------------------------- */}
-        <Brands10 />
-        <Properties10 />
-        <Properties2_10 />
-        <Properties3_10 />
-        <HelpSection10 />
-
-        {/* ---------------------------- */}
-        {/* Home 1 Section */}
-        {/* ---------------------------- */}
-        <Hero1 />
-        <Banner1 />
-        <About1 />
-        <Agents1 />
-        <Facts1 />
-        <Featured1 />
-        <FeaturedProperty1 />
-        <Cities1 />
-        <Categories1 />
-        <Features1 />
-        <HelpCenter1 />
-        <HelpSection1 />
-        <DownloadApp1 />
-        <LoanCalculator1 />
-        <Projects1 />
-        <Blogs1 />
-        <Brands1 />
-        <Cta1 />
-        <Contact1 />
-
-        {/* ---------------------------- */}
-        {/* Home 2 Section */}
-        {/* ---------------------------- */}
-        <Header2 />
-        <Home2Hero />
-        <div className="main-content">
-          <Home2Categories />
-          <Home2About />
-          <Home2Properties />
+          {/* Home 7 Content */}
+          <HighlightSection />
+          <HelpCenter />
+          <Contact7 />
+          {/* Facts from various homes */}
+          <Facts1 />
           <Home2Facts />
-          <Home2Agents />
-          <Home2Cities />
-          <Home2Testimonials />
-          <Home2Banner />
-          <Home2Brands />
-          <Home2Blogs />
-        </div>
-        <Footer1 logo="/images/logo/logo-3@2x.png" />
-
-        {/* ---------------------------- */}
-        {/* Home 8 Section */}
-        {/* ---------------------------- */}
-        <Header8 />
-        <Home8Hero />
-        <div className="main-content bg-color-white">
           <Home8Facts />
-          <Home8WelcomeSection />
-          <Home8FeaturedProperty />
-          <Home8Brands />
-          <Home8Properties />
-          <Home8Banner />
-          <Home8Testimonials />
-          <Home8Services />
-          <Home8Agents />
-          <Home8Blogs />
-        </div>
-        <Footer2 parentClass="style-2" />
-
-        {/* ---------------------------- */}
-        {/* Home 9 Section */}
-        {/* ---------------------------- */}
-        <Header9 />
-        <div className="bg-body-1 wrap-top">
-          <Home9Hero />
-        </div>
-        <div className="main-content">
           <Home9Facts />
-          <Home9About />
-          <Home9Services />
-          <Home9Properties />
-          <Home9Features />
-          <Home9Agents />
-          <Home9Services2 />
-          <Home9Testimonials />
+          {/* Categories, Cities, Banners */}
+          <Categories1 />
+          <Cities1 />
+          <Banner1 />
+          <Home2Banner />
+          <Home8Banner />
+          {/* Additional Home 1 Components */}
+          <DownloadApp1 />
+          <LoanCalculator1 />
+          <Projects1 />
+          <Cta1 />
+          <Contact1 />
+          {/* Brands from various homes */}
+          <Brands1 />
+          <Home2Brands />
+          <Home8Brands />
           <Home9Brands />
-          <Home9Blogs />
-        </div>
-        <Footer2 parentClass="style-2" />
+          {/* Home 10 Content */}
+          <Brands10 />
+          <HelpSection10 />
+          {/* Additional Help Sections */}
+          <HelpCenter1 />
+          <HelpSection1 />
+        </section>
 
         {/* ---------------------------- */}
-        {/* Global Footer */}
+        {/* Footers */}
         {/* ---------------------------- */}
-        <Footer2 />
+        <section id="footers">
+          <Footer2 />
+          <Footer1 logo="/images/logo/logo-3@2x.png" />
+          <Footer2 parentClass="style-2" />
+          <Footer2 />
+        </section>
+
       </div>
     </>
   );
